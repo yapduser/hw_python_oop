d9e45198df58ae44c3819d6315456230e6355b4f
yapduser <apronyaev@yandex.ru>
2023-05-02 02:22:51 +0300

    0

--- homework.py
+++ homework.py
@@ -6,6 +6,7 @@ import exceptions.errors as err
 @dataclass
 class InfoMessage:
     """Информационное сообщение о тренировке."""
+
     training_type: str
     duration: float
     distance: float
@@ -15,11 +16,11 @@ class InfoMessage:
     def get_message(self) -> str:
         """Вернуть сообщение с результатами."""
         info_msg: str = (
-            f'Тип тренировки: {self.training_type}; '
-            f'Длительность: {self.duration:.3f} ч.; '
-            f'Дистанция: {self.distance:.3f} км; '
-            f'Ср. скорость: {self.speed:.3f} км/ч; '
-            f'Потрачено ккал: {self.calories:.3f}.'
+            f"Тип тренировки: {self.training_type}; "
+            f"Длительность: {self.duration:.3f} ч.; "
+            f"Дистанция: {self.distance:.3f} км; "
+            f"Ср. скорость: {self.speed:.3f} км/ч; "
+            f"Потрачено ккал: {self.calories:.3f}."
         )
 
         return info_msg
@@ -28,6 +29,7 @@ class InfoMessage:
 @dataclass
 class Training:
     """Базовый класс тренировки."""
+
     LEN_STEP: ClassVar[float] = 0.65
     M_IN_KM: ClassVar[int] = 1000
     MIN_IN_H: ClassVar[int] = 60
@@ -47,7 +49,7 @@ class Training:
     def get_spent_calories(self) -> float:
         """Получить количество затраченных калорий."""
         raise NotImplementedError(
-            f'В дочернем классе {self.__class__.__name__}, метод не определен.'
+            f"В дочернем классе {self.__class__.__name__}, метод не определен."
         )
 
     def show_training_info(self) -> InfoMessage:
@@ -64,6 +66,7 @@ class Training:
 @dataclass
 class Running(Training):
     """Тренировка: бег."""
+
     SPEED_MULT: ClassVar[int] = 18
     SPEED_SHIFT: ClassVar[float] = 1.79
 
@@ -74,7 +77,10 @@ class Running(Training):
         )
 
         return (
-            speed_transform * self.weight / self.M_IN_KM * self.duration
+            speed_transform
+            * self.weight
+            / self.M_IN_KM
+            * self.duration
             * self.MIN_IN_H
         )
 
@@ -82,6 +88,7 @@ class Running(Training):
 @dataclass
 class SportsWalking(Training):
     """Тренировка: спортивная ходьба."""
+
     WEIGHT_MULT_1: ClassVar[float] = 0.035
     WEIGHT_MULT_2: ClassVar[float] = 0.029
     KMH_IN_MSEC: ClassVar[float] = 0.278
@@ -92,13 +99,12 @@ class SportsWalking(Training):
     def get_spent_calories(self) -> float:
         """Получить количество затраченных калорий."""
         speed_height_ratio: float = (
-            (self.get_mean_speed() * self.KMH_IN_MSEC) ** 2
-            / (self.height / self.CM_IN_M)
-        )
+            self.get_mean_speed() * self.KMH_IN_MSEC
+        ) ** 2 / (self.height / self.CM_IN_M)
 
         speed_weight_mult: float = (
-            self.WEIGHT_MULT_1 * self.weight + speed_height_ratio
-            * self.WEIGHT_MULT_2 * self.weight
+            self.WEIGHT_MULT_1 * self.weight
+            + speed_height_ratio * self.WEIGHT_MULT_2 * self.weight
         )
 
         return speed_weight_mult * self.duration * self.MIN_IN_H
@@ -107,6 +113,7 @@ class SportsWalking(Training):
 @dataclass
 class Swimming(Training):
     """Тренировка: плавание."""
+
     LEN_STEP: ClassVar[float] = 1.38
     SPEED_SHIFT: ClassVar[float] = 1.1
     SPEED_MULT: ClassVar[int] = 2
@@ -123,17 +130,19 @@ class Swimming(Training):
     def get_spent_calories(self) -> float:
         """Получить количество затраченных калорий."""
         return (
-            (self.get_mean_speed() + self.SPEED_SHIFT) * self.SPEED_MULT
-            * self.weight * self.duration
+            (self.get_mean_speed() + self.SPEED_SHIFT)
+            * self.SPEED_MULT
+            * self.weight
+            * self.duration
         )
 
 
 def read_package(workout_type: str, data: list[Union[int, float]]) -> Training:
     """Прочитать данные полученные от датчиков."""
     workout_data: dict[str, Type[Training]] = {
-        'RUN': Running,
-        'WLK': SportsWalking,
-        'SWM': Swimming,
+        "RUN": Running,
+        "WLK": SportsWalking,
+        "SWM": Swimming,
     }
 
     if workout_type not in workout_data:
@@ -147,16 +156,17 @@ def read_package(workout_type: str, data: list[Union[int, float]]) -> Training:
 
 
 def main(training: Training) -> None:
+    """Главная функция."""
     info_msg: InfoMessage = training.show_training_info()
     print(info_msg.get_message())
 
 
-if __name__ == '__main__':
-    # Тестовые пакеты данных
+if __name__ == "__main__":
+    # Тестовые пакеты данных.
     packages = [
-        ('SWM', [720, 1, 80, 25, 40]),
-        ('RUN', [15000, 1, 75]),
-        ('WLK', [9000, 1, 75, 180]),
+        ("SWM", [720, 1, 80, 25, 40]),
+        ("RUN", [15000, 1, 75]),
+        ("WLK", [9000, 1, 75, 180]),
     ]
 
     for workout_type, data in packages:
